a440d195881c2aad6eeaa6985c990b487ce14496
EmmyFortin <fortin.emmy@gmail.com>
2026-08-28 13:03:41 -0400

    reteste

--- P3/P3C2/énoncé/main.py
+++ P3/P3C2/énoncé/main.py
@@ -1,5 +1,4 @@
-import requests
-from bs4 import BeautifulSoup
+
 from bs4 import BeautifulSoup
 with open("index.html", "r", encoding="utf-8") as file:
     soup = BeautifulSoup(file, "html.parser")
